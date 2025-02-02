2db3879be9bb329849c3edde3a140bccaf1751a3
perdub <81479253+perdub@users.noreply.github.com>
2025-02-02 12:18:56 +0000

    add custom image to aot sdk

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 docker build --pull --rm -f "Dockerfile.sdk.alpine" -t perdub/dotnet-images:tmp1 "."
 docker build --pull --rm -f "Dockerfile.runtime.alpine" -t perdub/dotnet-images:tmp2 "."
+docker build --pull --rm -f "Dockerfile.aotsdk.alpine" -t perdub/dotnet-images:tmp3 "."
 
 docker tag perdub/dotnet-images:tmp1 perdub/dotnet-images:sdk-alpine-${GITHUB_RUN_ATTEMPT}
 docker tag perdub/dotnet-images:tmp1 perdub/dotnet-images:sdk-alpine-latest
 docker tag perdub/dotnet-images:tmp2 perdub/dotnet-images:runtime-alpine-${GITHUB_RUN_ATTEMPT}
 docker tag perdub/dotnet-images:tmp2 perdub/dotnet-images:runtime-alpine-latest
+docker tag perdub/dotnet-images:tmp3 perdub/dotnet-images:aot-sdk-alpine-${GITHUB_RUN_ATTEMPT}
+docker tag perdub/dotnet-images:tmp3 perdub/dotnet-images:aot-sdk-alpine-latest
 
 docker image push perdub/dotnet-images:sdk-alpine-${GITHUB_RUN_ATTEMPT}
 docker image push perdub/dotnet-images:sdk-alpine-latest
 docker image push perdub/dotnet-images:runtime-alpine-${GITHUB_RUN_ATTEMPT}
 docker image push perdub/dotnet-images:runtime-alpine-latest
+docker image push perdub/dotnet-images:aot-sdk-alpine-${GITHUB_RUN_ATTEMPT}
+docker image push perdub/dotnet-images:aot-sdk-alpine
