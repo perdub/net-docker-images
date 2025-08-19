1fc91124464cf3993d7aa1d7b2e0e58ec24689d4
perdub <81479253+perdub@users.noreply.github.com>
2025-08-19 20:23:41 +0000

    add ubuntu runtime

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ docker build --pull --rm -f "Dockerfile.runtime.alpine" -t perdub/dotnet-images:
 docker build --pull --rm -f "Dockerfile.aotsdk.alpine" -t perdub/dotnet-images:tmp3 "."
 
 docker build --pull --rm -f "Dockerfile.sdk.ubuntu" -t perdub/dotnet-images:tmp4 "."
+docker build --pull --rm -f "Dockerfile.runtime.ubuntu" -t perdub/dotnet-images:tmp5 "."
 
 docker tag perdub/dotnet-images:tmp1 perdub/dotnet-images:sdk-alpine-${GITHUB_RUN_ATTEMPT}
 docker tag perdub/dotnet-images:tmp1 perdub/dotnet-images:sdk-alpine-latest
@@ -11,7 +12,10 @@ docker tag perdub/dotnet-images:tmp2 perdub/dotnet-images:runtime-alpine-latest
 docker tag perdub/dotnet-images:tmp3 perdub/dotnet-images:aot-sdk-alpine-${GITHUB_RUN_ATTEMPT}
 docker tag perdub/dotnet-images:tmp3 perdub/dotnet-images:aot-sdk-alpine-latest
 docker tag perdub/dotnet-images:tmp4 perdub/dotnet-images:sdk-ubuntu-${GITHUB_RUN_ATTEMPT}
-docker tag perdub/dotnet-images:tmp4 perdub/dotnet-images:adk-ubuntu-latest
+docker tag perdub/dotnet-images:tmp4 perdub/dotnet-images:sdk-ubuntu-latest
+docker tag perdub/dotnet-images:tmp5 perdub/dotnet-images:runtime-ubuntu-${GITHUB_RUN_ATTEMPT}
+docker tag perdub/dotnet-images:tmp5 perdub/dotnet-images:runtime-ubuntu-latest
+
 
 docker image push perdub/dotnet-images:sdk-alpine-${GITHUB_RUN_ATTEMPT}
 docker image push perdub/dotnet-images:sdk-alpine-latest
@@ -20,4 +24,6 @@ docker image push perdub/dotnet-images:runtime-alpine-latest
 docker image push perdub/dotnet-images:aot-sdk-alpine-${GITHUB_RUN_ATTEMPT}
 docker image push perdub/dotnet-images:aot-sdk-alpine-latest
 docker image push perdub/dotnet-images:sdk-ubuntu-${GITHUB_RUN_ATTEMPT}
-docker image push perdub/dotnet-images:adk-ubuntu-latest
+docker image push perdub/dotnet-images:sdk-ubuntu-latest
+docker image push perdub/dotnet-images:runtime-ubuntu-${GITHUB_RUN_ATTEMPT}
+docker image push perdub/dotnet-images:runtime-ubuntu-latest
